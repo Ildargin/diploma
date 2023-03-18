e888c1a734983fc36ff04477e3e11179667e96d1
Ildargin <ildar.gainatullin@gmail.com>
2023-03-18 22:33:23 +0300

    style: run prettier

--- etheral/src/components/source-selector/infura-dialog/component.tsx
+++ etheral/src/components/source-selector/infura-dialog/component.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const InfuraDialog = ({ open, onClose, onSubmit }: Props) => {
   const [infuraToken, setInfuraToken] = useState('')
-  
+
   const onSave = () => {
     onSubmit({
       infuraToken,
